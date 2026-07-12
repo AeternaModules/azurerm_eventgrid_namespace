9d5f3ecec0430958f28ddb888e2a637d061064e5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "eventgrid_namespaces" {
-  description = "All eventgrid_namespace resources"
-  value       = azurerm_eventgrid_namespace.eventgrid_namespaces
-}
 output "eventgrid_namespaces_capacity" {
-  description = "List of capacity values across all eventgrid_namespaces"
-  value       = [for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : v.capacity]
+  description = "Map of capacity values across all eventgrid_namespaces, keyed the same as var.eventgrid_namespaces"
+  value       = { for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : k => v.capacity }
 }
 output "eventgrid_namespaces_identity" {
-  description = "List of identity values across all eventgrid_namespaces"
-  value       = [for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : v.identity]
+  description = "Map of identity values across all eventgrid_namespaces, keyed the same as var.eventgrid_namespaces"
+  value       = { for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : k => v.identity }
 }
 output "eventgrid_namespaces_inbound_ip_rule" {
-  description = "List of inbound_ip_rule values across all eventgrid_namespaces"
-  value       = [for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : v.inbound_ip_rule]
+  description = "Map of inbound_ip_rule values across all eventgrid_namespaces, keyed the same as var.eventgrid_namespaces"
+  value       = { for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : k => v.inbound_ip_rule }
 }
 output "eventgrid_namespaces_location" {
-  description = "List of location values across all eventgrid_namespaces"
-  value       = [for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : v.location]
+  description = "Map of location values across all eventgrid_namespaces, keyed the same as var.eventgrid_namespaces"
+  value       = { for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : k => v.location }
 }
 output "eventgrid_namespaces_name" {
-  description = "List of name values across all eventgrid_namespaces"
-  value       = [for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : v.name]
+  description = "Map of name values across all eventgrid_namespaces, keyed the same as var.eventgrid_namespaces"
+  value       = { for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : k => v.name }
 }
 output "eventgrid_namespaces_public_network_access" {
-  description = "List of public_network_access values across all eventgrid_namespaces"
-  value       = [for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : v.public_network_access]
+  description = "Map of public_network_access values across all eventgrid_namespaces, keyed the same as var.eventgrid_namespaces"
+  value       = { for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : k => v.public_network_access }
 }
 output "eventgrid_namespaces_resource_group_name" {
-  description = "List of resource_group_name values across all eventgrid_namespaces"
-  value       = [for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : v.resource_group_name]
+  description = "Map of resource_group_name values across all eventgrid_namespaces, keyed the same as var.eventgrid_namespaces"
+  value       = { for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : k => v.resource_group_name }
 }
 output "eventgrid_namespaces_sku" {
-  description = "List of sku values across all eventgrid_namespaces"
-  value       = [for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : v.sku]
+  description = "Map of sku values across all eventgrid_namespaces, keyed the same as var.eventgrid_namespaces"
+  value       = { for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : k => v.sku }
 }
 output "eventgrid_namespaces_tags" {
-  description = "List of tags values across all eventgrid_namespaces"
-  value       = [for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : v.tags]
+  description = "Map of tags values across all eventgrid_namespaces, keyed the same as var.eventgrid_namespaces"
+  value       = { for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : k => v.tags }
 }
 output "eventgrid_namespaces_topic_spaces_configuration" {
-  description = "List of topic_spaces_configuration values across all eventgrid_namespaces"
-  value       = [for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : v.topic_spaces_configuration]
+  description = "Map of topic_spaces_configuration values across all eventgrid_namespaces, keyed the same as var.eventgrid_namespaces"
+  value       = { for k, v in azurerm_eventgrid_namespace.eventgrid_namespaces : k => v.topic_spaces_configuration }
 }
 
